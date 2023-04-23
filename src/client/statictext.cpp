81687e69a95b99efc3ca911f6431a5fad5ef2819
Tofame <niewiadoma.65@gmail.com>
2023-04-23 10:27:11 +0200

    Offset for a text "Player says: ... "
    Offset, now the yellow text of the message is above the character, above the health bar.

--- src/client/statictext.cpp
+++ src/client/statictext.cpp
@@ -39,7 +39,7 @@ StaticText::StaticText()
 void StaticText::drawText(const Point& dest, const Rect& parentRect)
 {
     Size textSize = m_cachedText.getTextSize();
-    Rect rect = Rect(dest - Point(textSize.width() / 2, textSize.height()) + Point(20, 5), textSize);
+    Rect rect = Rect(dest - Point(textSize.width() / 2, textSize.height()) + Point(15, -5), textSize); // Tofame where text is written default: 20, 5
     Rect boundRect = rect;
     boundRect.bind(parentRect);
 
